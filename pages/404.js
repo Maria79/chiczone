da1a31c49f056ee9cc991767618a9d15beed70a4
Maria79 <mariajalbisu@gmail.com>
2023-04-05 17:22:18 +0100

    0504

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,12 @@
+import Layout from '../components/underConstruction/Layout';
 export default function Custom404() {
 	return (
-		<div className='max-w-full min-h-screen flex justify-center items-center'>
-			<h1 className='font-semibold max-w-md text-center'>
-				<span className='font-bold'>404</span> <br /> Page Not Found
-			</h1>
-		</div>
+		<Layout>
+			<div className='max-w-full min-h-screen flex justify-center items-center'>
+				<h1 className='font-semibold max-w-md text-center'>
+					<span className='font-bold'>404</span> <br /> Page Not Found
+				</h1>
+			</div>
+		</Layout>
 	);
 }
